8d65068fe5587caa9245910d32711d2d1fcc66a5
AlexDelei <alexdelei125@gmail.com>
2024-07-04 20:14:19 +0000

    replacing using regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-"""
-Regex-ing
-"""
+"""Regex-ing"""
 from typing import List
 import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    Using regex to replace certain occure
-    """
+    """Using regex to replace certain occure"""
     pattern = r'(' + '|'.join([f'{f}=[^{separator}]*' for f in fields]) + r')'
     return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
