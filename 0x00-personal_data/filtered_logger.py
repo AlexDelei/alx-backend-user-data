9b24b5abe32d87ef8562f39148c15cc47d10ce3d
AlexDelei <alexdelei125@gmail.com>
2024-07-04 20:03:36 +0000

    replacing using regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Using regex to replace certain occure
+    """
+    pattern = r'(' + '|'.join([f'{f}=[^{separator}]*' for f in fields]) + r')'
+    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
