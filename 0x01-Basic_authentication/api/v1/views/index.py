03e5cbad464ec91814a7266f1405030895fd1a9a
AlexDelei <alexdelei125@gmail.com>
2024-07-09 17:42:17 +0300

    403 - Forbidden Error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unaothorized_test() -> str:
       - 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden_test() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      - 403 error handler
+    """
+    abort(403)
