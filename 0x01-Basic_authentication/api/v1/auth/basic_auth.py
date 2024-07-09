463679b2f354c2b407efe17c8f2f27934e848bb8
AlexDelei <alexdelei125@gmail.com>
2024-07-09 22:19:36 +0300

    Extracting basic of base64 decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic auth
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -22,3 +23,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """Basic - Base64 decoding
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_b = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_b.decode('utf-8')
+        except Exception:
+            return None
+        return decoded_str
