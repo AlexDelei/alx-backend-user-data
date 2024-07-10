73a2e95793f59e5f778ea44b0d586d63a4de7bdd
AlexDelei <alexdelei125@gmail.com>
2024-07-10 05:58:33 +0300

    A working authentication system

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -83,3 +83,31 @@ class BasicAuth(Auth):
             return None
 
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retreiving a User instance from the request
+        """
+        # retreiving the authorization header from the request
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        
+        # Extract Base64 part of the authorization header
+        b64_auth = self.extract_base64_authorization_header(auth_header)
+        if b64_auth is None:
+            return None
+        
+        # Decode the base64 part
+        decode = self.decode_base64_authorization_header(b64_auth)
+        if decode is None:
+            return None
+        
+        # Extract the user credentials from (email, passwd)
+        u_email, u_pswd = self.extract_user_credentials(decode)
+        if u_email is None or u_pswd is None:
+            return None
+        
+        # retreiving user instance
+        user = self.user_object_from_credentials(u_email, u_pswd)
+        return user
