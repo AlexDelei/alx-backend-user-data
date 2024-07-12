2ca6000fa6f5da049c28c5405aca89b1c4002993
AlexDelei <alexdelei125@gmail.com>
2024-07-12 08:15:57 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Creating a new authentication mechanism
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    New authentication mechanism
+    """
+    pass
