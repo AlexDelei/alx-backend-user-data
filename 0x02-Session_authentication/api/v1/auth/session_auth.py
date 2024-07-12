5083e32f77f12b6c77338e0f38b1d458e634f17a
AlexDelei <alexdelei125@gmail.com>
2024-07-12 08:32:49 +0300

    A new session authentication mechanism

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 Creating a new authentication mechanism
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +10,23 @@ class SessionAuth(Auth):
     """
     New authentication mechanism
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Every user has a session id.
+
+        Args:
+            user_id - user's id as a string
+        Returns:
+            The cls attribute dictionary.
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return self.user_id_by_session_id
