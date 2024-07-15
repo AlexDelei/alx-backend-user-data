c6c6a20551484b7f26605940d4c5a26fc631773d
AlexDelei <alexdelei125@gmail.com>
2024-07-15 12:25:16 +0300

    Creating a Logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,24 @@ from typing import List
 import re
 import logging
 
+"""
+Write a function called filter_datum that returns the log message obfuscated:
+
+Arguments:
+fields: a list of strings representing all fields to obfuscate
+redaction: a string representing by what the field will be obfuscated
+message: a string representing the log line
+
+separator: a string representing by which character is separating
+all fields in the log line (message)
+
+The function should use a regex to replace occurrences of certain field values.
+filter_datum should be less than 5 lines long and use re.sub to
+perform the substitution with a single regex.
+"""
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(
         fields: List[str],
@@ -37,3 +55,69 @@ class RedactingFormatter(logging.Formatter):
         """Log formatter - when formatting"""
         msg = super().format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+"""
+Implement a get_logger function that
+takes no arguments and returns a logging.Logger object.
+
+The logger should be named "user_data" and
+only log up to logging.INFO level.
+It should not propagate messages to other loggers.
+It should have a StreamHandler with RedactingFormatter as formatter.
+
+Create a tuple PII_FIELDS constant at the root of the module
+containing the fields from user_data.csvthat are considered
+PII. PII_FIELDS can contain only 5 fields -
+choose the right list of fields that can are
+considered as “important” PIIs or information that you must hide in your logs.
+Use it to parameterize the formatter.
+"""
+"""
+PseudoCode:
+## Define the PII_FIELDS tuple
+STEP 1: Choosing the fields from user_data.csv that are considered PII
+
+EXPLANATION:
+The PII_FIELDS are defined at the root of the module
+to contain the fields considered PII
+
+
+## Implemeting the get_logger function
+STEP 1: Create a Logger named 'user_data'
+STEP 2: Set the logger level to logging.INFO
+STEP 3: Ensure the logger does not propagate messages to other loggers.
+STEP 4: Add a StreamHandler with RedactingFormatter as the formatter.
+STEP 5: Parameterize the formatter with the PII fields.
+
+
+EXPLANATION:
+Creates a logger named "user_data".
+Sets the logger level to logging.INFO.
+Disables message propagation to other loggers.
+Adds a StreamHandler with RedactingFormatter configured with PII_FIELDS.
+Returns the configured logger.
+
+"""
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creating a logger with certain requirements
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    # logger.propagate = False ; prevents messages from
+    # being propagated to other loggers
+
+    stream_handler = logging.StreamHandler()
+
+    # setting the formatter
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    # adding handler to the Logger
+    logger.addHandler(stream_handler)
+
+    return logger
