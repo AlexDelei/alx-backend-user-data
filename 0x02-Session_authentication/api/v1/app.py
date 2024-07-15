4b0fa1b4a867ff7c9410856fdf57ac7b3fc7551d
AlexDelei <alexdelei125@gmail.com>
2024-07-15 06:38:07 +0300

    fixing import issues

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,14 +10,11 @@ from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request, g
 from flask_cors import (CORS, cross_origin)
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = getenv("AUTH_TYPE", None)
-
-
 if auth == 'basic_auth':
     auth = BasicAuth()
 elif auth == 'session_auth':
