6f48cc63d7343aca205aab25aca9ba234ae91714
AlexDelei <alexdelei125@gmail.com>
2024-07-15 08:56:42 +0300

    import issues

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,20 +6,24 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request, g
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.auth import Auth
+
+auth = None
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 auth = getenv("AUTH_TYPE", None)
+
 if auth == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif auth == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 else:
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
@@ -36,16 +40,14 @@ def execute_before_request():
         ]
 
     req_auth = auth.require_auth(request.path, rq_lst)
-    auth_session_cookie = auth.session_cookie(request)
-    auth_header = auth.authorization_header(request)
 
     if req_auth:
-        if not auth_header and not auth_session_cookie:
+        auth_header = auth.authorization_header(request)
+        if auth_header is None and auth.session_cookie is None:
             abort(401)
         if not auth.current_user(request):
             abort(403)
-
-    g.current_user = auth.current_user(request)
+        g.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
