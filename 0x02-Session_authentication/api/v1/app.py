59462a50ef366a474c171d5f2e87c47f66a8000d
AlexDelei <alexdelei125@gmail.com>
2024-07-15 09:15:26 +0300

    reducing importation issues

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,27 +3,25 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request, g
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.auth import Auth
-
-auth = None
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-
-auth = getenv("AUTH_TYPE", None)
-
-if auth == 'basic_auth':
+# Lazy import of authentication classes to avoid circular import issues
+auth = None
+auth_type = getenv("AUTH_TYPE", None)
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth == 'session_auth':
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 else:
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
@@ -37,7 +35,7 @@ def execute_before_request():
     rq_lst = [
         '/api/v1/status/', '/api/v1/unauthorized/',
         '/api/v1/forbidden/', '/api/v1/auth_session/login/'
-        ]
+    ]
 
     req_auth = auth.require_auth(request.path, rq_lst)
 
