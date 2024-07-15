82712e78117eb916a1b271757751eb2db7945dc6
AlexDelei <alexdelei125@gmail.com>
2024-07-15 16:05:19 +0300

    lets test it out

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -2,6 +2,7 @@
 """
 Main file
 """
+import logging
 
 get_db = __import__('filtered_logger').get_db
 RedactingFormatter = __import__('filtered_logger').RedactingFormatter
@@ -18,7 +19,9 @@ records = cursor.fetchall()
 
 for record in records:
     formatted_record = "; ".join(f"{col}='{val}'" for col, val in zip(columns, record)) + ";"
-    print(formatted_record)
+    log_record = logging.LogRecord("my_logger", logging.INFO, None, None, formatted_record, None, None)
+    formatter = RedactingFormatter(fields=("name", "email", "phone", "ssn", "password"))
+    print(formatter.format(log_record))
 
 
 cursor.close()
