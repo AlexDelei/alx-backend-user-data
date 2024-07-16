2998b6a7e92bfd19a127babe268fc855c87917e5
AlexDelei <alexdelei125@gmail.com>
2024-07-16 09:48:16 +0300

    Created a User Model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Creating a User model and mapping it
+using sqlalchemy
+"""
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+    )
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User Model
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
