a26381b392d69f6f4f21420f0154652e3174d0c7
AlexDelei <alexdelei125@gmail.com>
2024-07-16 11:26:14 +0300

    get User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import (
+    NoResultFound, InvalidRequestError, ArgumentError
+    )
 
 from user import Base, User
 
@@ -42,3 +45,22 @@ class DB:
         self._session.commit()
 
         return u_obj
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Performs Query and filters down by the
+        arguments provided
+
+        Args:
+            keyword arguments
+        Return:
+            User object found
+        """
+        try:
+            u_data = self._session.query(User).filter_by(**kwargs).first()
+            if u_data is None:
+                raise NoResultFound
+            return u_data
+
+        except ArgumentError:
+            raise InvalidRequestError
