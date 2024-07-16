b10d6584ea8e94d3929e986363f7ab6d31457cc2
AlexDelei <alexdelei125@gmail.com>
2024-07-16 16:10:01 +0300

    Register User

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 Basic Flask App
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -14,6 +16,19 @@ def basic_app():
     Returns a simple payload using jsonify
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=['POST'])
+def users():
+    """
+    Endpoint for registering a User
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
