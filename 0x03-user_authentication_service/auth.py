80ca20cdc24a1c4d34f3e4dd5ccd9422556a3016
AlexDelei <alexdelei125@gmail.com>
2024-07-16 22:23:08 +0300

    Has an issue, will try and fix it later

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,6 +64,24 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """
+        Finds the user using the email, generates
+        uuid and stores it in the db
+
+        Args:
+            email - user email
+        Returns:
+            session id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            user_session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=user_session_id)
+            return user_session_id
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """
