951be1725737681844784a302ceb6e8840ec5882
AlexDelei <alexdelei125@gmail.com>
2024-07-16 15:09:15 +0300

    Adding authentication systems

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Authentication systems
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,3 +18,30 @@ def _hash_password(password: str) -> bytes:
         a byte string of the hashed password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt(14))
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register's a user if does not exist in our
+        database
+
+        Args:
+            email - user email, wil be used for validation
+            password - user password
+        Returns:
+            Saved user object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+        raise ValueError("User <user's email> already exists")
