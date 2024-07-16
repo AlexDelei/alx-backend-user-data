c8088221205407fe35723fb7b4400ff1e26b5346
AlexDelei <alexdelei125@gmail.com>
2024-07-16 12:50:55 +0300

    Type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,7 +61,7 @@ class DB:
             raise InvalidRequestError
         return user
 
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
+    def update_user(self, user_id: int, **kwargs):
         """
         Locates the user to update using the user_id arg
 
