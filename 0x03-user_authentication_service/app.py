b636dd005471d86e138580c4665954072a0c53b5
AlexDelei <alexdelei125@gmail.com>
2024-07-18 05:46:41 +0300

    Logout a User by deleting session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask App
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -44,6 +44,19 @@ def login():
         AUTH.create_session(email)
         return jsonify({"email": email, "message": "logged in"})
     abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    Logout by deleting user session id
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
