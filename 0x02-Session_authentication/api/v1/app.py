fc58616a16b55b061a9062c91288f4734610cbd8
AlexDelei <alexdelei125@gmail.com>
2024-07-21 15:37:47 +0300

    proper route authentication logic

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,7 +3,7 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify, abort, request, g
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 
@@ -11,22 +11,17 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Lazy import of authentication classes to avoid circular import issues
-auth = None
+
 auth_type = getenv("AUTH_TYPE", None)
-# print("Authentication Variable: ", auth_type)
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-    # print("Authentication system choosed: ", auth.__class__.__name__)
 elif auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-    # print("Authentication systems choosed: ", auth)
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
-    # print("Authentication system choosed: ", auth)
 
 
 @app.before_request
@@ -34,23 +29,21 @@ def execute_before_request():
     """
     Request Validation before making any requests
     """
-    if auth is None:
-        return
-
-    rq_lst = [
-        '/api/v1/status/', '/api/v1/unauthorized/',
-        '/api/v1/forbidden/', '/api/v1/auth_session/login/'
-    ]
-
-    req_auth = auth.require_auth(request.path, rq_lst)
-
-    if req_auth:
-        auth_header = auth.authorization_header(request)
-        if auth_header is None and auth.session_cookie is None:
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
-        request.current_user = auth.current_user(request)
+    if auth:
+        paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
+        if auth.require_auth(request.path, paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None or auth.session_cookie is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 @app.errorhandler(404)
@@ -76,5 +69,5 @@ def forbiddenError(error) -> str:
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", 5000)
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
