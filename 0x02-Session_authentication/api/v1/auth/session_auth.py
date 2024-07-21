1c3d5471f78d5f75ad98fe6a07e429c2002adff2
AlexDelei <alexdelei125@gmail.com>
2024-07-21 17:08:21 +0300

    Retreive the user id using the session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,13 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retreiving the user id using the
+        session id as the key
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        user = self.user_id_by_session_id.get(session_id)
+        return user
